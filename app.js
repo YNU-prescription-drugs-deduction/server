eb5043ddcc9833d553c49a9e6859aba807072c2c
Yoo <hiyoojeong@gmail.com>
2023-02-17 20:01:16 +0900

    [refactor] change routing address

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const userRouter = require('./src/user/userRoute');
 const itemRouter = require('./src/item/itemRoute');
 
 app.use(express.json());
-app.use('/v1', userRouter);
-app.use('/v1/item', itemRouter);
+app.use('/v1/users', userRouter);
+app.use('/v1/items', itemRouter);
 
 module.exports = app;
