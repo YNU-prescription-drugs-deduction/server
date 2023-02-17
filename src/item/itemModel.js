54562b794c34fabe288e6477a2fe68457ac836ba
Yoo <hiyoojeong@gmail.com>
2023-02-17 23:27:21 +0900

    [style] change date column from 'date' to 'string'

--- src/item/itemModel.js
+++ src/item/itemModel.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+const todayString = require('../../util/date');
 
 const Item = new mongoose.Schema({
     userId: { type: String, ref: 'User', required: true },
-    date: { type: Date, default: new Date(), require: true },
+    date: { type: String, default: todayString, require: true },
     status: { type: String, default: "접수", require: true },
     accountBank: { type: String, require: true }, // 은행
     accountName: { type: String, require: true }, // 예금주
